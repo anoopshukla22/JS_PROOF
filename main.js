414beb3c094e7da05538b4ad36f38f1b39ea31ba
anoopshukla22 <99082946+anoopshukla22@users.noreply.github.com>
2023-07-22 11:38:53 +0530

    Update main.js
    JS Proof assignment

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
-
 // create a variable to hold your NFT's
-
+const NFTs = [];
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
+function mintNFT (_name, _city, _age, _cloth, _clothColor) {
+    const NFT = {
+        "name" : _name,
+        "city" : _city,
+        "age"  : _age,
+        "cloth": _cloth,
+        "clothColor": _clothColor,
+    }
+    NFTs.push(NFT);
+    console.log("Minted :" + _name);
 
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
+    for(let i = 0; i<NFTs.length; i++){
+        console.log("\nID  :\t\t" + i+1);
+        console.log("Name :\t\t" + NFTs[i].name);
+        console.log("City :\t\t" + NFTs[i].city);
+        console.log("Age  :\t\t" + NFTs[i].age);
+        console.log("Cloth :\t\t" + NFTs[i].cloth);
+        console.log("Cloth Color:" + NFTs[i].clothColor);
+    }
 
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
+    console.log("\n"+NFTs.length);
 
 }
 
 // call your functions below this line
+mintNFT("Young","Mumbai","20","Shirt","Blue");
+mintNFT("Angle","Pune","17","T-Shirt","Yellow");
+mintNFT("Zubair","Chandigarh","25","Shirt","Light Green");
 
+listNFTs();
+getTotalSupply();
